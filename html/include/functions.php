0b30749d9e20529b3407ade326347ca0482fdc1b
Jackey <jziwenchen@gmail.com>
2014-06-22 23:13:53 +0800

    .

--- html/include/functions.php
+++ html/include/functions.php
@@ -8,9 +8,7 @@ else {
 
 define("ROOT_PATH", dirname('../'.__FILE__));
 
-print ROOT_PATH."/../admin/yii/yii.php";
-
-require_once(ROOT_PATH."/admin/yii/yii.php");
+require_once(ROOT_PATH."/../admin/yii/yii.php");
 
 $config = ROOT_PATH.'/../admin/protected/config/main.php';
 
