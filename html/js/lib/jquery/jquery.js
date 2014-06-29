4b9570d59ef91d5a0b8aded31a333867fa9a392d
hdg <hdg1988@gmail.com>
2014-06-29 23:59:11 +0800

    fix

--- html/js/lib/jquery/jquery.js
+++ html/js/lib/jquery/jquery.js
@@ -3,6 +3,7 @@ if( !window.jQuery ) {
         'lib/order!lib/jquery/jquery.min',
         'lib/order!lib/jquery/jquery.fancybox',
         'lib/order!lib/jquery/jquery.jcarousel',
+        'lib/order!lib/jquery/jquery.hammer',
         'lib/order!lib/jquery/mediaelement-and-player.min',
         'lib/order!lib/jquery/jquery.ChinaCitySelect'
     ], function() {
