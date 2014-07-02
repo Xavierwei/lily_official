5512c61c0fea22b8f061f8f50999aef25ba3f23d
hdg <hdg1988@gmail.com>
2014-07-03 02:11:08 +0800

    fxi

--- html/js/common/loading.js
+++ html/js/common/loading.js
@@ -113,16 +113,149 @@ define([
 
         }
 
-        imgLoad.on('always', function(instance) {
-            dTop.queue(function() {
-                dTop.dequeue();
+        var _finisherTimer = setInterval(function(){
+            if( !_finished['top'] ) return
 
-                // try to init the map
-                map.init();
+            clearInterval( _finisherTimer );
+            // dTop.dequeue();
 
-                var dHeight = $(window).height() - 90*2;
+
+            // try to init the map
+            map.init();
+
+            dWrap.fadeIn();
+
+            // show tapes
+            dTape.fadeIn();
+
+//                    setTimeout(function(){
+//                        localHash();
+//                    },1000);
+
+            if (oSkrollr) {
+                oSkrollr.refresh();
+            } else {
+                // let elements skroll
+                oSkrollr = skrollr.init({
+                    edgeStrategy: 'set',
+                    forceHeight: false
+                })
+            }
+        } , 1000 / 20 );
+
+//         imgLoad.on('always', function(instance) {
+//             dTop.queue(function() {
+//                 var _timer = setInterval(function(){
+//                     if( !_finished['top'] ) return
+
+//                     clearInterval( _timer );
+//                     dTop.dequeue();
+
+
+//                     // try to init the map
+//                     map.init();
+
+//                     dWrap.fadeIn();
+
+//                     // show tapes
+//                     dTape.fadeIn();
+
+//     //                    setTimeout(function(){
+//     //                        localHash();
+//     //                    },1000);
+
+//                     if (oSkrollr) {
+//                         oSkrollr.refresh();
+//                     } else {
+//                         // let elements skroll
+//                         oSkrollr = skrollr.init({
+//                             edgeStrategy: 'set',
+//                             forceHeight: false
+//                         })
+//                     }
+//                 } , 1000 / 20 );
+//     //             setTimeout(function(){
+//     //                 dTop.dequeue();
+
+
+//     //                 // try to init the map
+//     //                 map.init();
+
+//     //                 dWrap.fadeIn();
+
+//     //                 // show tapes
+//     //                 dTape.fadeIn();
+
+//     // //                    setTimeout(function(){
+//     // //                        localHash();
+//     // //                    },1000);
+
+//     //                 if (oSkrollr) {
+//     //                     oSkrollr.refresh();
+//     //                 } else {
+//     //                     // let elements skroll
+//     //                     oSkrollr = skrollr.init({
+//     //                         edgeStrategy: 'set',
+//     //                         forceHeight: false
+//     //                     })
+//     //                 }
+//     //             } , 1.5 * nTime);
+                
+
+// //                 var dHeight = $(window).height() - 90*2;
+// //                 dRight.animate({
+// //                     height: dHeight
+// //                 } , 200);
+// //                 dBottom
+// //                     .delay(200)
+// //                     .animate({
+// //                         'width':'90%'
+// //                     } , 200);
+
+// //                 dLeft.delay(400)
+// //                     .animate({
+// //                         'height':dHeight
+// //                     } , 200);
+
+// //                 dTop.delay(600).animate({
+// //                     'width': '90%'
+// //                 }, nTime, function() {
+// //                     // show the page content
+// //                     dWrap.fadeIn();
+
+// //                     // show tapes
+// //                     dTape.fadeIn();
+
+// // //                    setTimeout(function(){
+// // //                        localHash();
+// // //                    },1000);
+
+// //                     if (oSkrollr) {
+// //                         oSkrollr.refresh();
+// //                     } else {
+// //                         // let elements skroll
+// //                         oSkrollr = skrollr.init({
+// //                             edgeStrategy: 'set',
+// //                             forceHeight: false
+// //                         })
+// //                     }
+// //                 })
+//             })
+//         })
+
+        $(window).resize(function(){
+            var dHeight = $(window).height() - 90*2;
+            dRight.css('height', dHeight);
+            dLeft.css('height', dHeight);
+        });
+        var _animate = {};
+        var _finished = {};
+        var _dHeight = $(window).height() - 90*2;
+        var _timer = setInterval(function(){
+            if( !nTotal ){
+                clearInterval( _timer );
                 dRight.animate({
-                    height: dHeight
+                    height: _dHeight
                 } , 200);
                 dBottom
                     .delay(200)
@@ -132,86 +265,166 @@ define([
 
                 dLeft.delay(400)
                     .animate({
-                        'height':dHeight
+                        'height':_dHeight
                     } , 200);
 
-                dTop.delay(600).animate({
-                    'width': '90%'
-                }, nTime, function() {
-                    // show the page content
-                    dWrap.fadeIn();
 
-                    // show tapes
-                    dTape.fadeIn();
+                dTop.delay(600)
+                    .animate({
+                        'width': '90%'
+                    }, nTime , '' , function() {
+                        _finished['top'] = 1;
+                    });
 
-//                    setTimeout(function(){
-//                        localHash();
-//                    },1000);
+                return;
+            }
 
-                    if (oSkrollr) {
-                        oSkrollr.refresh();
-                    } else {
-                        // let elements skroll
-                        oSkrollr = skrollr.init({
-                            edgeStrategy: 'set',
-                            forceHeight: false
-                        })
-                    }
-                })
-            })
-        })
 
-        $(window).resize(function(){
-            var dHeight = $(window).height() - 90*2;
-            dRight.css('height', dHeight);
-            dLeft.css('height', dHeight);
+            var nVal = parseInt((nLoad / nTotal) * 100);
+            if (0 < nVal && nVal < 25) {
+                dRight.stop( true , true )
+                    .animate({
+                        'height': _dHeight * nVal / 25
+                    } , nTime );
+                return;
+            }
+            if( !_finished['right'] ){
+                dRight.animate({
+                    height: _dHeight
+                } , nTime / 2 , '' , function(){
+                    _finished['right'] = 1;
+                });
+                return;
+            }
+
+            if (25 < nVal && nVal < 50 ) {
+                dBottom.stop( true , true )
+                    .animate({
+                        'width': (nVal - 25) / 25 * 90 + '%'
+                    } , nTime); 
+                return;
+            }
+            if( !_finished['bottom'] ){
+                dBottom.animate({
+                    width: '90%'
+                } , nTime / 2 , '' , function(){
+                    _finished['bottom'] = 1;
+                });
+                return;
+            }
+
+            if (50 < nVal && nVal < 75) {
+
+                dLeft.stop(true , true)
+                    .animate({
+                        'height': (nVal - 50)/ 25 * _dHeight
+                    } , nTime);
+
+                return;
+            }
+            if( !_finished['left'] ){
+                dLeft.animate({
+                    height: _dHeight
+                } , nTime / 2 , '' , function(){
+                    _finished['left'] = 1;
+                });
+                return;
+            }
+
+            if ( 75 < nVal && nVal <= 100 && !_finished['top'] ) {
+                dTop.stop(true , true)
+                    .animate({
+                        'width': (nVal - 75) / 25 * 90 + '%'
+                    } , nTime , '' , function(){
+                        if( nVal == 100 ){
+                            _finished['top'] = 1;
+                            clearInterval( _timer );
+                        }
+                    });
+                return;
+            }
+            // if( !_finished['top'] ){
+            //     dTop.animate({
+            //         width: 900
+            //     } , nTime / 2 , '' , function(){
+            //         _finished['top'] = 1;
+            //     });
+            //     clearInterval( _timer );
+            //     return;
+            // }
+        } , nTime / 2 );
+        imgLoad.on('progress', function(instance, image) {
+            nLoad += 1;
+            // var dHeight = $(window).height() - 90*2;
+            // var nVal = parseInt((nLoad / nTotal) * 100);
+
+            // if( nVal > 25 && !_animate['top'] ){
+            //     _animate['top'] = 1;
+            //     dTop.animate({
+            //         'width': '90%'
+            //     } , nTime);
+            // }
+            // if( nVal > 50 && !_animate['right'] ){
+            //     _animate['right'] = 1;
+            //     dRight.animate({
+            //         'height': dHeight
+            //     } , nTime );
+            // }
+            // if( nVal > 75 && !_animate['bottom'] ){
+            //     _animate['bottom'] = 1;
+            //     dBottom.animate({
+            //         'width': '90%'
+            //     } , nTime );
+            // }
+            // if( nVal > 99 && !_animate['left'] ){
+            //     _animate['left'] = 1;
+            //     dLeft.animate({
+            //         'height': dHeight
+            //     } , nTime );
+            // }
+            // if (0 < nVal && nVal < 25) {
+            //     dRight.stop( true , true )
+            //         .animate({
+            //             'height': dHeight * nVal / 25
+            //         } , nTime );
+            //     return;
+            // }
+            // if (25 < nVal && nVal < 50 ) {
+            //     dRight.css({
+            //         'height': dHeight
+            //     });
+
+            //     dBottom.css({
+            //         'width': parseInt((nVal / 50) * 100) + '%'
+            //     }); 
+
+            //     return;
+            // }
+
+            // if (50 < nVal && nVal < 75) {
+            //     dBottom.css({
+            //         'width': '90%'
+            //     })
+
+            //     dLeft.css({
+            //         'height': parseInt((nVal / 75) * 100) + '%'
+            //     })
+
+            //     return;
+            // }
+
+            // if (75 < nVal && nVal < 100) {
+            //     dLeft.css({
+            //         'height': '75%'
+            //     })
+
+            //     dTop.animate({
+            //         'width': parseInt((nVal / 100) * 100) + '%'
+            //     }, nTime)
+
+            //     return;
+            // }
         });
-        // imgLoad.on('progress', function(instance, image) {
-        //     nLoad += 1;
-        //     var nVal = parseInt((nLoad / nTotal) * 100);
-        //     if (0 < nVal && nVal < 25) {
-        //         dRight.css({
-        //             'height': dHeight * nVal / 25
-        //         });
-        //         return;
-        //     }
-        //     if (25 < nVal && nVal < 50 ) {
-
-        //         dRight.css({
-        //             'height': dHeight
-        //         });
-
-        //         dBottom.css({
-        //             'width': parseInt((nVal / 50) * 100) + '%'
-        //         }); 
-
-        //         return;
-        //     }
-
-        //     if (50 < nVal && nVal < 75) {
-        //         dBottom.css({
-        //             'width': '90%'
-        //         })
-
-        //         dLeft.css({
-        //             'height': parseInt((nVal / 75) * 100) + '%'
-        //         })
-
-        //         return;
-        //     }
-
-        //     if (75 < nVal && nVal < 100) {
-        //         dLeft.css({
-        //             'height': '75%'
-        //         })
-
-        //         dTop.animate({
-        //             'width': parseInt((nVal / 100) * 100) + '%'
-        //         }, nTime)
-
-        //         return;
-        //     }
-        // })
     }
 
     return {
